6858af707ce8579945ce66a50d321018b973365a
Gustavo Amorim <gustavoamorilsilva@gmail.com>
2026-04-20 15:13:09 -0300

    FEAT: implementação de solicitações, swagger e ajuste de localização

--- TaskTrack.Api/Controllers/SolicitacoesController.cs
+++ TaskTrack.Api/Controllers/SolicitacoesController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using TaskTrack.Application.DTOs;
+using TaskTrack.Application.Interfaces;
 
 namespace TaskTrack.Api.Controllers;
 
@@ -6,9 +8,42 @@ namespace TaskTrack.Api.Controllers;
 [Route("api/[controller]")]
 public class SolicitacoesController : ControllerBase
 {
-    [HttpGet]
-    public IActionResult GetAll()
+    private readonly ISolicitacoesService _solicitacoesService;
+
+    public SolicitacoesController(ISolicitacoesService solicitacoesService)
     {
-        return Ok(Array.Empty<object>());
+        _solicitacoesService = solicitacoesService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+    {
+        var solicitacoes = await _solicitacoesService.GetAllAsync(cancellationToken);
+        return Ok(solicitacoes);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        var solicitacao = await _solicitacoesService.GetByIdAsync(id, cancellationToken);
+        return solicitacao is null ? NotFound() : Ok(solicitacao);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateSolicitacaoRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var created = await _solicitacoesService.CreateAsync(request, cancellationToken);
+            return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(new { message = ex.Message });
+        }
     }
 }
